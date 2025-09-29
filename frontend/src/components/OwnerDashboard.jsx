b76d789d489f9f8934aac8b2e3f19de28a9152f1
Lucky Chauhan <126170989+luckyChauhan12w@users.noreply.github.com>
2025-09-30 02:50:19 +0530

    Update OwnerDashboard.jsx

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Nav from "./NaV.JSX";
+import Nav from "./Nav.JSX";
 import { useSelector } from "react-redux";
 import { FaUtensils, FaSpinner } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
